09dca7d68e37d74e2afc1e6a8fd69b7fcbade99d
yasin gökçe şahin <48259029+yasingokce@users.noreply.github.com>
2021-08-18 17:27:18 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.LevelLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(377, 297);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(29, 29);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(347, 492);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 32);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scorelabel.ForeColor = System.Drawing.Color.White;
+            this.scorelabel.Location = new System.Drawing.Point(12, 9);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(67, 24);
+            this.scorelabel.TabIndex = 2;
+            this.scorelabel.Text = "score :";
+            // 
+            // LevelLabel
+            // 
+            this.LevelLabel.AutoSize = true;
+            this.LevelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LevelLabel.ForeColor = System.Drawing.Color.White;
+            this.LevelLabel.Location = new System.Drawing.Point(883, 9);
+            this.LevelLabel.Name = "LevelLabel";
+            this.LevelLabel.Size = new System.Drawing.Size(79, 24);
+            this.LevelLabel.TabIndex = 3;
+            this.LevelLabel.Text = "Bölüm 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(984, 536);
+            this.Controls.Add(this.LevelLabel);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.ball);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "tugla kirma";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownE);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyUpE);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Label LevelLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft;
+        bool goRight;
+        bool isGameOver;
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+        PictureBox[] bricksArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PlaceBlocks();
+            SetupGame();
+        }
+        // Tuğla oluşturma
+        public void PlaceBlocks()
+        {
+            bricksArray = new PictureBox[24];
+
+            int a = 0;
+            int top = 50;
+            int left = 100;
+            // her bir tuğlanın özellikleri(boy, en, tagname) ve sıralanması durumu
+            for (int i = 0; i < bricksArray.Length; i++)
+            {   
+                bricksArray[i] = new PictureBox();
+                bricksArray[i].Height = 30;
+                bricksArray[i].Width = 100;
+                bricksArray[i].Tag = "bricks";
+                bricksArray[i].BackColor = Color.Brown;
+                // ekran eni: 1000, her bir tuğlanın uzunluğu: 100, ilk boşluk: 100 aradaki boşluk sayısı: 5
+                // tuğlalar 600+ ilk boşluk 100+ ara boşluklar 30*5:150+ son boşluk 150
+                // satır atla 
+                if (a == 6)
+                {
+                    // tuğla eni 30 arada kalacak boşluk 20
+                    top = top + 50; // üstten satır atla 
+                    left = 100;
+                    a = 0;
+                }
+                // satıra diz
+                if (a < 6)
+                {
+                    a++;
+                    bricksArray[i].Left = left;
+                    bricksArray[i].Top = top;
+                    this.Controls.Add(bricksArray[i]); // tuğla oluştur
+                    left = left + 130;  // tuğla boyu ve aradaki boşluk değeri
+
+                }
+
+            }
+
+        }
+
+        public void SetupGame()
+        {
+            isGameOver = false;
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 15;
+            scorelabel.Text = "Score: " + score;
+            //top başlangıç 
+            ball.Left = 300;
+            ball.Top = 300;
+            // raket başlangıç yeri
+            player.Left = 500;
+            timer.Start();
+
+
+        }
+       
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+            // oyuncunun ekran boyutunu aşmayacak şekilde sağa ve sola gitme hareketi 
+            scorelabel.Text = "Score: " + score;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= playerSpeed;
+            }
+
+            if (goRight == true && player.Left < 900)
+            {
+                player.Left += playerSpeed;
+            }
+            // top başlangıç hareketi
+            ball.Left += ballx;
+            ball.Top += bally;
+            // topun ekran boyutunu aşmayacak şekilde sağa ve sola gitme hareketi 
+            if (ball.Left < 0 || ball.Left > 950)
+            {
+                ballx = -ballx;
+            }
+            if (ball.Top < 0)
+            {
+                bally = -bally;
+            }
+            // topun oyuncu ile çarpışma durumu
+            if (ball.Bounds.IntersectsWith(player.Bounds))
+            {
+                // topun oyuncuya çarpma durumundaki yön ve hızı
+                bally = 8 * -1;
+
+                if (ballx < 0)
+                {
+                    ballx = 8 * -1;
+                }
+                else
+                {
+                    ballx = 8;
+                }
+            }
+            // topun tuğlaya çarpma durumu
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "bricks")
+                {
+                    if (ball.Bounds.IntersectsWith(x.Bounds)) 
+                    {
+                        score += 1;
+
+                        bally = -bally; 
+                     //   ballx = -ballx;
+
+                        this.Controls.Remove(x);
+                    }
+                }
+
+            }
+
+
+            if (score == 24)
+            {
+                PlaceBlocks();
+                LevelLabel.Text = "Bölüm 2";
+            }
+
+            if (score == 48)
+            {
+                gameOver("Kazandınız!! Tekrar başlamak için Enter tuşuna basın.");
+            }
+
+            if (ball.Top > 580)
+            {
+                gameOver("Kaybettiniz... Tekrar başlamak için Enter tuşuna basın.");
+            }
+
+
+
+        }
+
+        // baştan başlatma durumunda her tuğlanın silinmesi gerek
+        private void RemoveBricks()
+        {
+            foreach (PictureBox x in bricksArray)
+            {
+                this.Controls.Remove(x);
+            }
+        }
+
+        private void KeyDownE(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+        }
+
+
+        private void KeyUpE(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RemoveBricks();
+                PlaceBlocks();
+                SetupGame();
+                LevelLabel.Text = "Bölüm 1";
+            }
+
+        }
+
+        public void gameOver(string message)
+        {
+            isGameOver = true;
+            timer.Stop();
+            scorelabel.Text = "Score: " + score + " " + message;
+        }
+    }
+}
